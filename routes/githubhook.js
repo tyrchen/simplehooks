85bb56b35f95e36df4156264264786113d16a681
tchen <tchen@tukeq.com>
2013-01-09 17:59:04 +0800

    fix typo and add log

--- routes/githubhook.js
+++ routes/githubhook.js
@@ -9,14 +9,14 @@ var _ = require('underscore');
 var repos = [
     {name: 'cayman', ref: 'master', action: deploy},
     {name: 'cayman', ref: 'live', action: log},
-    {name: 'barr', ref: 'master', action: deploy}
+    {name: 'barr', ref: 'master', action: log}
 
 ];
 
 
 function deploy(repo, ref, data) {
     console.log('deploy:', repo, ref);
-    var cmd = '~/bin/github_' + repos + '_' + ref;
+    var cmd = '/home/dev/bin/github_' + repo + '_' + ref;
     exec(cmd, function(err, output) { log(repo, ref, data); console.log(err); });
 }
 
@@ -40,9 +40,10 @@ exports.githubhook = function(req, res){
     }
 
     _.each(repos, function(repo) {
-       if(repo.name === payload.repository.name && payload.ref.indexOf(repo.ref) >= 0) {
-           repo.action(repo.name, repo.ref, payload);
-       }
-       res.send('ok');
+        console.log(payload.repository.name, payload.ref, repo.name, repo.ref);
+        if(repo.name === payload.repository.name && payload.ref.indexOf(repo.ref) >= 0) {
+            repo.action(repo.name, repo.ref, payload);
+        }
+        res.send('ok');
     });
 };
